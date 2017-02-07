b543aa72ef5a6e3774edac01536c7e97bbe47bc0
Renato <marenato@inf.ethz.ch>
2017-02-07 14:42:40 +0100

    Complete experiment setup

--- src/main/resources/exp_config.py
+++ src/main/resources/exp_config.py
@@ -1,11 +1,12 @@
 class Config:
     exec_path   = '/mnt/SG/marenato/kafka_2.11-0.10.1.0'
     #exec_path   = ''
-    num_parts   = 16
+    partitions  = 16
     sf          = 1
     tuples_sf   = 5
     bid_ratio   = 2
     zk_dir      = '/tmp/zookeeper'
     zk_config   = 'zookeeper.properties'
-    kafkadir    = '/tmp/kafka-logs'
+    k_dir   	= '/tmp/kafka-logs'
+    k_config	= 'server.properties'
     server      = 'localhost'
--- src/main/resources/setup.py
+++ src/main/resources/setup.py
@@ -23,6 +23,7 @@ if 'threading' in sys.modules:
 logging.basicConfig()
 
 def confZk():
+    # TODO configure zk properties file?
     deleteClient = ThreadedClients([Config.server], "rm -rf {0}".format(Config.zk_dir), root=True)
     deleteClient.start()
     deleteClient.join()
@@ -33,14 +34,39 @@ def startZk():
     zkClient = ThreadedClients([Config.server], zk_cmd, root=True, observers=[observer])
     print zk_cmd
     zkClient.start()
-    # observer.waitFor(1)
+    observer.waitFor(1)
     return zkClient
 
 def startKafka():
+    # remove data folder
+    deleteClient = ThreadedClients([Config.server], "rm -rf {0}".format(Config.k_dir), root=True)
+    deleteClient.start()
+    deleteClient.join()
+
+    # Start Kafka
+    observer = Observer("started (kafka.server.KafkaServer)")
+    k_cmd = '{0}/bin/kafka-server-start.sh {0}/config/{1}'.format(Config.exec_path, Config.k_config)
+    kClient = ThreadedClients([Config.server], k_cmd, root=True, observers=[observer])
+    kClient.start()
+    observer.waitFor(1)
+    return kClient
+
+def startSetup():
+    # start zk
     zkClient = startZk()
     # start kafka
+    kafkaClient = startKafka()
     # create topics
-    return [zkClient]
+    createTopic('bid-topic', Config.partitions, Config.exec_path, Config.server)
+    createTopic('auction-topic', Config.partitions, Config.exec_path, Config.server)    
+    return [kafkaClient] + [zkClient]
+
+def createTopic(tName, nParts, ePath, server):
+    # TODO Change zk default port?
+    ct_cmd = '{0}/bin/kafka-topics.sh --create --partitions {1} --zookeeper {2}:2181 --replication-factor 1 --topic {3}'
+    topicClient = ThreadedClients([Config.server], ct_cmd.format(ePath, nParts, server, tName), root=True)
+    topicClient.start()
+    topicClient.join()
 
 def exitGracefully(storageClients, signal, frame):
     try:
@@ -60,7 +86,7 @@ def exitGracefully(storageClients, signal, frame):
     #exit(0)
 
 if __name__ == "__main__":
-    expClients = startKafka()
+    expClients = startSetup()
     signal.signal(signal.SIGINT, partial(exitGracefully, expClients))
     print "\033[1;31mExp started\033[0m"
     print "\033[1;31mHit Ctrl-C to shut it down\033[0m"
