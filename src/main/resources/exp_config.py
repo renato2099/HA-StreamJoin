29cfbf4b3bead4f6f6983c96054141d24e0d3ef1
Renato Marroquin <renatoj.marroquin@gmail.com>
2017-02-10 17:05:27 +0100

    Adding configs

--- src/main/resources/exp_config.py
+++ src/main/resources/exp_config.py
@@ -3,10 +3,19 @@ class Config:
     exec_path   = '/Users/renatomarroquin/Documents/Apache/Kafka/kafka_2.11-0.10.1.0'
     partitions  = 16
     sf          = 1
-    tuples_sf   = 5
+    tuples      = 5
     bid_ratio   = 2
     zk_dir      = '/tmp/zookeeper'
     zk_config   = 'zookeeper.properties'
     k_dir       = '/tmp/kafka-logs'
     k_config    = 'server.properties'
     server      = 'localhost'
+    missing     = 0
+    pcompletion = 0.9
+    psuccess    = 0.1
+    aproducer   = 'auction-producer'
+    bproducer   = 'bid-producer'
+    pjoin       = 'pjoin'
+    hajoin      = 'hajoin'
+    jarpath     = ''
+
