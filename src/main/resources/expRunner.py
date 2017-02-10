728a4c804582334c268606ee839dd014bdbd0127
Renato <marenato@inf.ethz.ch>
2017-02-10 17:06:06 +0100

    Adding on experiment runner

--- src/main/resources/expRunner.py
+++ src/main/resources/expRunner.py
@@ -7,7 +7,7 @@ from threaded_ssh import ThreadedClients
 from observer import *
 from exp_config import *
 from setup import *
-
+from argparse import ArgumentParser
 
 import logging
 
@@ -20,14 +20,40 @@ if 'threading' in sys.modules:
 
 logging.basicConfig()
 
+def populate():
+   aParams = "kafka={0}:9092 zk={0}:2181 missing={1} sf={2} tuples={3} pcompletion={4} psuccess={5}".format(Config.server, Config.missing, Config.tuples, Config.pcompletion, Config.psuccess)
+   
+   aClient = ThreadedClients([Config.server], "java -jar {0}/{1}.jar {2}".format(Config.jarpath, Config.aproducer, aParams))
+   aClient.start()
+   aClient.join()
+
+def execApp(approach):
+    print approach
+
+def runExperiments(approaches):
+    for a in approaches:
+        print "###############################"
+        print "######### " + a + " ###########"
+        print "###############################"
+        # setup
+        clients = startSetup()
+        time.sleep(2)
+        # run
+        print "experiments with some settings"
+        populate()
+        execApp(a)
+        # kill running services
+        for c in clients:
+            c.kill()
+        # stop
+        stopSetup()
+        
+
 if __name__ == "__main__":
-    # stop
-    logging
-    stopSetup()
-    # setup
-    clients = startSetup()
-    # run
-    print "experiments with some settings"
-    # kill running services
-    for c in clients:
-        c.kill()
+    parser = ArgumentParser()
+    parser.add_argument("-l", dest='l', help="Location", default='l')
+    parser.add_argument("-a", dest='approaches', type=str, nargs='*', help="Approaches")
+    args = parser.parse_args()
+    if (args.l == 'h'):
+        Config.exec_path = Config.exec_path_h
+    runExperiments(args.approaches)
--- src/main/resources/exp_config.py
+++ src/main/resources/exp_config.py
@@ -1,6 +1,7 @@
 class Config:
-    #exec_path   = '/mnt/SG/marenato/kafka_2.11-0.10.1.0'
-    exec_path   = '/Users/renatomarroquin/Documents/Apache/Kafka/kafka_2.11-0.10.1.0'
+    exec_path_l = '/mnt/SG/marenato/kafka_2.11-0.10.1.0'
+    exec_path_h = '/Users/renatomarroquin/Documents/Apache/Kafka/kafka_2.11-0.10.1.0'
+    exec_path   = exec_path_l
     partitions  = 16
     sf          = 1
     tuples_sf   = 5
